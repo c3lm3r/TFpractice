4f57e09188affe2a11efbdcd84d6db46a5bbaa40
c3lm3r <justin.celmer@gmail.com>
2023-10-29 14:45:39 -0400

    added variable to main.tf and a the variable.tf file

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -14,11 +14,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0dbc3d7bc646e8516"
+  ami           = "ami-0fc5d935ebf8bc3bc"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = var.instance_name
   }
 }
 
